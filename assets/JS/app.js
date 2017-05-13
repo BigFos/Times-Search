707cd4536576776d1867c202c5b74e0d15162d65
Steven Foster Jr <Steven Foster Jr>
2017-05-13 13:37:49 -0700

    updated js

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -1,5 +1,5 @@
 var apiKey = "664e215dd5a34926b033d389481d8095"
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + apiKey + "&q=";
 
 $.ajax({
   url: queryURL,
