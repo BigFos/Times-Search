f7b5a7f9ec1674fd47b5c0ba89c61c5a31fa1f9e
James Wick <James Wick>
2017-05-13 13:56:02 -0700

    added for loop

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -3,13 +3,17 @@ var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key
 searchTerm = "india";
 var x = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + apiKey + "&q=" + searchTerm;
 console.log(x);
-
+// numResults = 5;
 
 $.ajax({
   url: x,
   method: 'GET',
 }).done(function(result) {
+	var numResults = $('.numResults').attr('value');
   console.log(result);
+  for (var i=0; i<numResults; i++) {
+  console.log(result.response.docs[i].headline.main);
+}
 }).fail(function(err) {
   throw err;
 });
